8dfd2748fd5ddf1bd53fbba610ac91262cc297e9
Vitalii Chernelivskyi <chernelivskyi@gmail.com>
2022-01-29 11:34:25 +0200

    skrol

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+const anchors = document.querySelectorAll('a[href^="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault()
+        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+        document.querySelector(goto).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
