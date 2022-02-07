4459fcb6746f8e7f580360f527bc5a14915db250
Vitalii Chernelivskyi <chernelivskyi@gmail.com>
2022-02-07 15:57:46 +0200

    fix  scroll

--- app/js/script.js
+++ app/js/script.js
@@ -13,17 +13,17 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-// const anchors = document.querySelectorAll('a.anchor')
-// for (let anchor of anchors) {
-//     anchor.addEventListener("click", function (e) {
-//         e.preventDefault()
-//         const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
-//         document.querySelector(goto).scrollIntoView({
-//             behavior: "smooth",
-//             block: "start"
-//         })
-//     })
-// }
+const anchors = document.querySelectorAll('a.anchor')
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault()
+        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+        document.querySelector(goto).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
 
 $('#exampleModal').on('shown.bs.modal', function () {
     $('#name').focus();
