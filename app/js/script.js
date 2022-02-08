f47d87fa98c8877ad1236021327fc17ddbb84b8f
Denys Skorov <denys.skorov@gmail.com>
2022-02-08 16:53:08 +0200

    fix

--- app/js/script.js
+++ app/js/script.js
@@ -51,55 +51,104 @@ jQuery.validator.addMethod("customEmail", function (value, element) {
 }, "Please enter valid email address!");
 
 
-$("#change-1").click(function () {
-    $('div#message').each(function () {
+// $("#change-1").click(function () {
+//     $('div#message').each(function () {
+//
+//         $(this).replaceWith(`<form  id='my-form' class='d-flex justify-content-between flex-nowrap valid-my'><input name="email" value="jon@mail.com" class='ml-2 error-valid'> <button class='btn' type="submit">Save</button></frorm>`);
+//         $("#my-form").validate({
+//             rules: {
+//                 email: {
+//                     required: true,
+//                     customEmail: true
+//                 }
+//             },
+//             submitHandler: function (form) {
+//                 alert('ok')
+//                 // form.submit();
+//             }
+//         });
+//     });
+//
+// });
 
-        $(this).replaceWith(`<form  id='my-form' class='d-flex justify-content-between flex-nowrap valid-my'><input name="email" value="jon@mail.com" class='ml-2 error-valid'> <button class='btn' type="submit">Save</button></frorm>`);
-        $("#my-form").validate({
-            rules: {
-                email: {
-                    required: true,
-                    customEmail: true
-                }
-            },
+
+// $("#change-2").click(function () {
+$('.double-form-item').each(function () {
+    const _this = this
+    $(this).find('button.double-form-change').click(function () {
+        // console.log(_this)
+        const div = $(_this).find('.double-form-text')
+        const type = div.attr('data-type')
+        const name = div.attr('data-name')
+        const value = div.text()
+        const form = $(`
+            <form class='double-form-change_form d-flex justify-content-between flex-nowrap'>
+                <input class="ml-2" type="${type}" id="form_save" name="${name}" value="${value}" > 
+                <button class='btn black' id="save" type="submit">Save</button>
+            </frorm>
+            `)
+
+        div.replaceWith(form);
+
+        const rules = {
+            [name]: {
+                required: true,
+                // email: true
+            }
+        }
+        debugger
+
+        //TODO якщо type === `email` -> підставити в rules[name][type] = true
+
+        // rules[name] = true
+
+        console.log(rules)
+
+        $(form).validate({
+            rules,
+            // rules: {
+            //     blabla: {
+            //         required: true,
+            //         // email: true
+            //         //    TODO: with type (example "email: true")
+            //     }
+            // },
             submitHandler: function (form) {
-                alert('ok')
+                debugger
+                form.replaceWith(div)
+                // alert('send')
                 // form.submit();
             }
+
         });
-    });
+
+    })
+
+    // $(this).replaceWith(`<form  id='my-form-1' class='d-flex justify-content-between flex-nowrap valid-my'><input class="ml-2" id="form_save" name="address" value="" > <button class='btn' id="save" type="submit">Save</button></frorm>`);
+    // $("#my-form-1").validate({
+    //     rules: {
+    //         address: {
+    //             required: true,
+    //         }
+    //     },
+    //
+    //
+    // });
+    //
+    // // $(
+    // //     function () {
+    // $('#save').click(function () {
+    //     let text = $('#form_save').val();
+    //     $('message-1').text(text);
+    // });
+    // }
+    // );
+
 
 });
 
 
-$("#change-2").click(function () {
-    $('div #message-1').each(function () {
-
-        $(this).replaceWith(`<form  id='my-form-1' class='d-flex justify-content-between flex-nowrap valid-my'><input class="ml-2" id="form_save" name="address" value="" > <button class='btn' id="save" type="submit">Save</button></frorm>`);
-        $("#my-form-1").validate({
-            rules: {
-                address: {
-                    required: true,
-                }
-            },
-
-
-        });
-
-        // $(
-        //     function () {
-        $('#save').click(function () {
-            let text = $('#form_save').val();
-            $('message-1').text(text);
-        });
-        // }
-        // );
-
-
-    });
-
-
-});
+// });
 
 
 // TODO for pc scroll
